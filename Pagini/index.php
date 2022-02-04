c3d035dd0f43567b0bb3d993be348e916fc46257
Hefaistos69 <69504268+Hefaistos69@users.noreply.github.com>
2022-02-04 22:56:57 +0200

    Initial commit

---
+++ Pagini/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php
+    include_once "../Module/modul-css.php";
+  ?>
+  <title>Pornire</title>
+</head>
+  <body>
+    
+
+  <?php
+    include_once "../Module/modul-js.php";
+  ?>
+  </body>
+</html>
