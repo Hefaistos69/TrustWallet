0b65bf1222750877fee71605b8940886d2a9f841
Hefaistos69 <69504268+Hefaistos69@users.noreply.github.com>
2022-02-08 19:47:46 +0200

    Update pagina-login.php

--- Pagini/pagina-login.php
+++ Pagini/pagina-login.php
@@ -18,7 +18,7 @@
         <div class="h-auto w-80 ">
           <h1 class="mt-5 text-light text-center">Autentificare</h1>
           <hr class="text-light mb-3">
-          <form action="">
+          <form action="" method="post">
             <div class="mb-2">
               <label for="username" class="form-label text-light fs-4">
                 Utilizator sau email
@@ -31,8 +31,10 @@
               </label>
               <input type="password" class="form-control bg-secondary border-0" id="password" name="password" placeholder="Parolă">
             </div>
-            <div class="text-end">
-              <button class="mb-1 btn btn-success btn-lg text-light" type="submit">Autentificare</button>
+            <div class="d-flex flex-row-reverse mb-1">
+              <button class="mx-1 btn btn-success btn-lg text-light" type="submit">Autentificare</button>
+              <button class="mx-1 btn btn-success btn-lg text-light" type="submit">Demo</button>
+              <button class="mx-1 btn btn-success btn-lg text-light" type="submit">Deținător</button>
             </div>  
             <div class="d-flex flex-column mb-5">
               <p class="fs-5 text-light mb-0">Nu ai cont? <a class="text-success fs-5 text-decoration-none" href="">Crează nou</a></p>
